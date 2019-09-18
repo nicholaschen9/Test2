c0008d49bdd277a4df8e7a2197654e5326e70169
nicholas_chen <nicholas_chen@worldvision.org.tw>
2019-09-18 15:52:48 +0800

    version 2

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -6,5 +6,8 @@ public class Hello {
 //      new Person().hello();
         Person p = new Person();
         p.hello();
+
+        int age = 19;
+        Integer age2= 19;
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -1,10 +1,16 @@
 package com.tom
 
+import java.awt.geom.Rectangle2D
+
 fun main(args: Array<String>) {
 //    println("Hello Kotlin")
 //    Human().hello()
     val h = Human()
     h.hello()
+
+    var age = 16
+    var age2     : Float = 16f
+
 }
 
 class Human {
